6b7ca082724f1c0171273880e6c7f47ba577dbd3
Ryan Block <rblock@gmail.com>
2019-01-25 13:39:46 -0800

    Implements simple child process queueing system Why, you may ask? Because Docker containers crash if you spawn too many child processes too quickly because reasons loldocker

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
 const child = require('child_process').spawn
 const path = require('path')
+
 /**
  * Add to your .arc file:
  *
  * @plugins
- * architect/plugin-node-prune?
+ * architect/plugin-node-prune
  *
  * Receives params:
  * {arc, pathToCode, env[, pluginConfig]}
  */
+
+// Setup
+let running = 0
+const limit = 10
+
 module.exports = { beforeDeploy }
 
 function beforeDeploy(params) {
   return new Promise((resolve, reject) => {
-    const cwd = process.cwd()
-    let {pathToCode} = params
-    pathToCode = pathToCode.startsWith(cwd)
-      ? pathToCode
-      : path.join(cwd, pathToCode)
-    const cmd = path.join(cwd, 'node_modules', '@architect', 'arc-plugin-node-prune', 'dist', 'node-prune')
-    const args = [pathToCode]
-    const options = {cwd, shell:true}
-    const spawn = child(cmd, args, options)
-    spawn.on('exit', function exit(code) {
-      if (code !== 0) {
-        reject('Prune error, code:', code)
+    function run() {
+      if (!(running < limit)) {
+        setTimeout(run, 500)
       }
       else {
-        return resolve()
+        running += 1
+        const cwd = process.cwd()
+        let {pathToCode} = params
+        pathToCode = pathToCode.startsWith(cwd)
+          ? pathToCode
+          : path.join(cwd, pathToCode)
+        const cmd = path.join(cwd, 'node_modules', '@architect', 'arc-plugin-node-prune', 'dist', 'node-prune')
+        const args = [pathToCode]
+        const options = {cwd, shell:true}
+        const spawn = child(cmd, args, options)
+        spawn.on('exit', function exit(code) {
+          running -= 1
+          if (code !== 0) {
+            reject('Prune error, code:', code)
+          }
+          else {
+            return resolve()
+          }
+        })
+        spawn.on('error', function error(err) {
+          running -= 1
+          reject(err)
+        })
       }
-    })
-    spawn.on('error', function error(err) {
-      reject(err)
-    })
+    }
+    run()
   })
 }
+
+
